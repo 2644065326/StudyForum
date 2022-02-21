bbe51345879fd677e17d2057bfee8a216f3b27a9
jiaminZhang <2644065326@qq.com>
2022-02-21 11:16:02 +0800

    删掉ARouter的测试代码

--- app/src/main/java/com/hbsf/app/MainActivity.java
+++ app/src/main/java/com/hbsf/app/MainActivity.java
@@ -4,25 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.hbsf.arouter_annotation.ARouter;
-import com.hbsf.arouter_annotation.Parameter;
-import com.hbsf.arouter_api.manager.RouterManager;
 
-
-@ARouter(path = "/app/MainActivity")
 public class MainActivity extends AppCompatActivity {
-    @Parameter
-    String name;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RouterManager.getInstance()
-                .build("/login/LoginActivity")
-                .withString("name", "zjm")
-                .navigation(this);
     }
 }
